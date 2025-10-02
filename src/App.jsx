4fda1e5e40b710a2c77901f122ad94a842b9f302
nikita <hembromnikita16@gmail.com>
2025-10-02 16:47:58 +0530

    Added hero background

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-
       <div className='pattern'/>
 
-      <div className='wrapper'>
+      <div className='wrapper'>  
         <header>
+          <img src="/hero.png" alt="Hero Banner" />
           <h1>Find <span className='text-gradient'>Movies </span>You'll Enjoy Without The Hassel</h1>
         </header>
 
