dbe0162b2f7ec7b86123594c204ec7282adb0393
nikita <hembromnikita16@gmail.com>
2025-10-02 15:21:02 +0530

    Added changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#030014',
+        light: {
+          100: '#cecefb',
+          200: '#a8b5db',
+        },
+        gray: {
+          100: '#9ca4ab',
+        },
+        dark: {
+          100: '#0f0d23',
+        },
+      },
+      fontFamily: {
+        'dm-sans': ['DM Sans', 'sans-serif'],
+        'bebas': ['Bebas Neue', 'sans-serif'],
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/hero-bg.png')",
+      },
+      screens: {
+        xs: '480px',
+      },
+    },
   },
   plugins: [],
 }
