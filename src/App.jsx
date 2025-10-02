ed44a80d89c8f77c9baafe13a678696e3d36f8b0
nikita <hembromnikita16@gmail.com>
2025-10-02 14:59:24 +0530

    Imported tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+      Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
 
 
 
